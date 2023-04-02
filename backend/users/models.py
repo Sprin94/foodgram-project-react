9bac2d36e14b18f9d22c0252a00f89b6b81bceb9
Andrei Rostovtsev <105871945+Sprin94@users.noreply.github.com>
2023-04-02 11:47:35 +0800

    fetch(users): create users app and model

---
+++ backend/users/models.py
@@ -0,0 +1,58 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.core.exceptions import ValidationError
+from django.core.mail import send_mail
+from django.db import models
+
+
+def username_not_me(username):
+    if username == 'me':
+        raise ValidationError('username не может быть "me"')
+
+
+class User(AbstractUser):
+    "Пользовательская модель юзера"
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        validators=[username_validator, username_not_me],
+    )
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=150,
+    )
+    last_name = models.CharField(
+        verbose_name='Фамилия',
+        max_length=150,
+    )
+    email = models.EmailField(
+        verbose_name='Email',
+        unique=True,
+        max_length=254,
+    )
+
+    def __str__(self):
+        return self.username
+
+    class Meta(AbstractUser.Meta):
+        ordering = ('-id',)
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='follower',
+        on_delete=models.CASCADE)
+    following = models.ForeignKey(
+        User,
+        related_name='following',
+        on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'following'],
+                name='unique_follow')
+        ]
