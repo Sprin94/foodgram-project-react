121d44e9a527c9bd36f88d98d34ca7772cbccfc7
Andrei Rostovtsev <105871945+Sprin94@users.noreply.github.com>
2023-04-02 16:52:44 +0800

    fetch(users): add UserSerializer

---
+++ backend/users/api/serializers.py
@@ -0,0 +1,58 @@
+from rest_framework.serializers import (
+    ModelSerializer, SlugRelatedField, CurrentUserDefault, ValidationError,
+    SerializerMethodField
+)
+from rest_framework.validators import UniqueTogetherValidator
+
+from users.models import User, Follow
+
+
+class UserSerializer(ModelSerializer):
+    is_subscribed = SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'is_subscribed'
+        )
+
+    def get_is_subscribed(self, obj):
+        user = self.context.get('request').user
+        if isinstance(user, User):
+            return Follow.objects.filter(user=user, following=obj).exists()
+        return False
+
+
+class FollowSerializer(ModelSerializer):
+    user = SlugRelatedField(
+        slug_field='username',
+        queryset=Follow.objects.all(),
+        default=CurrentUserDefault()
+    )
+    following = SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+
+    class Meta:
+        model = Follow
+        fields = '__all__'
+        read_only_fields = ('user',)
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=['user', 'following'],
+                message='Вы уже подписаны.'
+            )
+        ]
+
+    def validate(self, attrs):
+        if self.context.get('request').user == attrs.get('following'):
+            raise ValidationError(
+                'Нельзя подписаться на самого себя')
+        return super().validate(attrs)
